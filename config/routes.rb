0a501277bb21844dc2111c56c7f1cbe52ed67457
HoshinoHitomi <hiikochan711@gmail.com>
2021-07-20 07:59:13 +0000

    [Add]管理者側のtopページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions',
-    passwords: 'admins/passwords',
-    registrations: 'admins/registrations'
+
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
   }
+
   devise_for :customers, controllers: {
     sessions: 'customers/sessions',
     passwords: 'customers/passwords',
     registrations: 'customers/registrations'
   }
+
+  namespace :admin do
+    root "homes#top"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

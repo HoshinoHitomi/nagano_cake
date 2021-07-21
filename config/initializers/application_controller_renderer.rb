fd7a26429d7fc8c7c87f89164ac58b1852ffb3fc
HoshinoHitomi <hiikochan711@gmail.com>
2021-07-21 22:00:54 +0000

    [Add]商品詳細画面、編集機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9baf2fc8e00766681342afd6ebcfcfadd697dc9d76507c4967d5fb49ed53412ed734b543391898cd3875e42a7644bc2030cb33d183136fb136d330567822d6b5'

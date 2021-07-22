1a0da194c994a0d937ccdd7220fbaf46b340f26b
HoshinoHitomi <hiikochan711@gmail.com>
2021-07-22 16:02:24 +0000

    [Update]deviseのルーティングの見直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,28 @@
 Rails.application.routes.draw do
 
-  devise_for :admin, controllers: {
-    sessions: 'admin/sessions',
-    passwords: 'admin/passwords',
-    registrations: 'admin/registrations'
-  }
+  devise_for(
+    :admins,
+    path: 'admin',
+    module: 'admin/admins'
+  )
 
-  devise_for :customers, controllers: {
-    sessions: 'customers/sessions',
-    passwords: 'customers/passwords',
-    registrations: 'customers/registrations'
-  }
+  devise_for(
+    :customers,
+    path: 'customers',
+    module: 'public/customers'
+  )
 
   namespace :admin do
+
     root "homes#top"
     resources :items, except: [:destroy]
     resources :genres, only: [:index, :create, :edit, :update]
+  end
+
+
+  scope module: :public do
+    root to: "homes#top"
+    resources :items, only: [:index, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

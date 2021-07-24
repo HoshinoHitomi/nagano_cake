a951776022acf158e9770ec6144d6dab11c4a0ed
HoshinoHitomi <hiikochan711@gmail.com>
2021-07-24 23:14:22 +0000

    [Update]カートに数量、合計金額を表示

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,35 +7,48 @@ class Public::CartItemsController < ApplicationController
     session[:cart_item].each do |cart_item|
       item = Item.find_by(id: cart_item["item_id"])
       sub_total = item.price * cart_item["amount"].to_i
-
       next unless item
 
       @cart_item.push({ item_id: item.id,
-                        name: item.item_name,
+                        name: item.name,
                         price: item.price,
                         amount: cart_item["amount"].to_i,
                         sub_total: sub_total
       })
     end
 
-    @cart_total_price = cart_total_price(@cart_item)
+    @cart_item_total_price = cart_item_total_price(@cart_item)
 
   end
 
+  def cart_item_total_price(cart_item)
+    cart_item.sum { |hash| hash[:sub_total] }
+  end
+
+
   def create
+
     if session[:cart_item].blank?
+
       session[:cart_item] = [ { item_id: params["item_id"], amount: params["amount"].to_i } ]
       return redirect_to cart_items_path
+
     end
 
     match = session[:cart_item].select { |cart_item| cart_item["item_id"] == params["item_id"] }
 
     if match.present?
+
       match[0]["amount"] += params["amount"].to_i
+
     else
+
       session[:cart_item].push( { item_id: params["item_id"], amount: params["amount"].to_i } )
+
     end
+
     redirect_to cart_items_path
+
   end
 
   def update

1528ef8d6f04ab756156162c0345e651e0689f98
HoshinoHitomi <hiikochan711@gmail.com>
2021-07-30 18:35:49 +0000

    [Update]注文確認画面でカート内のアイテムを表示できるようにした

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,9 +12,28 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_item = []
 
-    @cart_items = Item.find(session[:cart_item])
+    session[:cart_item].each do |cart_item|
 
+      item = Item.find_by(id: cart_item["item_id"])
+
+      sub_total = item.price * cart_item["amount"].to_i
+
+      next unless item
+
+      @cart_item.push({ item_id: item.id,
+
+                        name: item.name,
+
+                        price: item.price,
+
+                        amount: cart_item["amount"].to_i,
+
+                        sub_total: sub_total
+
+      })
+    end
   end
 
   def create
